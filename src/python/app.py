dc7d3b649e18db82d87371b33b366fea031a6092
Buddhi Kavindra <visitbudkavin@gmail.com>
2022-12-01 00:02:12 +0530

    adding some python code

--- src/python/app.py
+++ src/python/app.py
@@ -8,3 +8,22 @@ def add():
     print(c)
 
 add()
+
+print(type(a))
+
+msg = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(msg)
+print(msg[10])
+
+def loopBanana():
+  for x in "banana":
+    print(x)
+
+print(len(msg))
+
+print("labore" in msg)
+
+print(msg[2:5])
